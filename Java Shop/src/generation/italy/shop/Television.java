46fdec3a664fc60891f4da232cef130bcbe99abb
Vincenzo Cafasso <103500666+vincaf@users.noreply.github.com>
2022-11-18 15:58:38 +0100

    feat: create Television class that extends Prodotto

--- Java Shop/src/generation/italy/shop/Television.java
+++ Java Shop/src/generation/italy/shop/Television.java
@@ -1,5 +1,12 @@
 package generation.italy.shop;
 
-public class Television {
-
+public class Television extends Prodotto{
+	private int sizeInches;
+	private boolean smart;
+	
+	Television(int code, String name, String brand, double price, int iva, int sizeInches, boolean smart){
+		super(code, name, brand, price, iva);
+		this.sizeInches = sizeInches;
+		this.smart = smart;
+	}
 }
