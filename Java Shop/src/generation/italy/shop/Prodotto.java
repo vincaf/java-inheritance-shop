1065c4f0e51b32c830d4e541959c159ca36e054f
Vincenzo Cafasso <103500666+vincaf@users.noreply.github.com>
2022-11-18 15:09:39 +0100

    feat: add getters and setters

--- Java Shop/src/generation/italy/shop/Prodotto.java
+++ Java Shop/src/generation/italy/shop/Prodotto.java
@@ -6,12 +6,52 @@ public class Prodotto {
 	private String name;
 	private String brand;
 	private double price;
-	private int iva = 22;
+	private int iva;
 	
-	public Prodotto(int code, String name, String brand, double price) {
+	public Prodotto(int code, String name, String brand, double price, int iva) {
 		this.code = code;
 		this.name = name;
 		this.brand = brand;
 		this.price = price;
+		this.iva = iva;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getIva() {
+		return iva;
+	}
+
+	public void setIva(int iva) {
+		this.iva = iva;
+	}
+
+	public int getCode() {
+		return code;
+	}
+	
+	
+	
 }
